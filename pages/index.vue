4f586e77ca38c3c7ef97f4a5aa296c5f661e5940
Aus Gomez <austrebertog@gmail.com>
2020-09-19 09:50:16 -0500

    init

---
+++ pages/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <section class="section">
+    <b-button type="is-primary">Primary</b-button>
+    <div class="buttons">
+      <button class="button is-info">Info</button>
+      <button class="button is-success">Success</button>
+      <button class="button is-warning">Warning</button>
+      <button class="button is-danger">Danger</button>
+    </div>
+  </section>
+</template>
+
+<script>
+import Card from '~/components/Card'
+
+export default {
+  name: 'HomePage',
+
+  components: {
+    Card,
+  },
+}
+</script>
