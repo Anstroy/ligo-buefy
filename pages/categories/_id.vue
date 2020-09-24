8bbaad1a48b72332e9106c1a398300402d9df1a1
Aus Gomez <austrebertog@gmail.com>
2020-09-23 21:52:43 -0500

    adding stuff

---
+++ pages/categories/_id.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <nuxt-child :category="category" />
+  </div>
+</template>
+
+<script>
+import categories from '@/data/categories'
+export default {
+  asyncData({ params }) {
+    return {
+      category: categories.find((c) => c.id === parseInt(params.id)),
+    }
+  },
+}
+</script>
+
+<style></style>
